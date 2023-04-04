ff967e554826d78bd58bbd21cfa73e3856d480d1
maki <mkr.fb-66@cameo.plala.or.jp>
2023-04-04 21:44:11 +0900

    ハンガリアン記法の変数を訂正。コードの再フォーマット

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -2,18 +2,19 @@ package com.example.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @RestController
 public class HelloWorldController {
     @GetMapping("/hello")
-    public String today(){
-        LocalDate  today = LocalDate.now();
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        String strToday = today.format(dtf);
+    public String today() {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter display = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        String todayDisplay = today.format(display);
         LocalDate newDay = today.plusDays(120);
-        String strNewDay = newDay.format(dtf);
-        return String.format("Hello world!今日は%s、120日後は%s",strToday,strNewDay);
+        String newDayDisplay = newDay.format(display);
+        return String.format("Hello world!今日は%s、120日後は%s", todayDisplay, newDayDisplay);
     }
-    }
+}
