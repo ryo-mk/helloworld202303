fc8714d172fd45e4b506aead2105c8e9a6fe050a
maki <mkr.fb-66@cameo.plala.or.jp>
2023-04-04 20:38:52 +0900

    今日の日付と、120日後の日付を追加

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -2,11 +2,18 @@ package com.example.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class HelloWorldController {
     @GetMapping("/hello")
-        public String hello() {
-            return "hello maki";
-        }
+    public String today(){
+        LocalDate  today = LocalDate.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        String strToday = today.format(dtf);
+        LocalDate newDay = today.plusDays(120);
+        String strNewDay = newDay.format(dtf);
+        return String.format("Hello world!今日は%s、120日後は%s",strToday,strNewDay);
+    }
     }
